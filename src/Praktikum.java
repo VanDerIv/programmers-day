769d7ded8e2a5d138b9a8969c9bd1c24feca979a
ivandr <ivandr@bk.ru>
2022-09-29 18:42:58 +0300

    first commit

---
+++ src/Praktikum.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Praktikum {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите год:");
+        int year = scanner.nextInt();
+
+        String leapDateStr = "13.09."+year;
+        if (isLeapYear(year)) {
+            leapDateStr = "12.09."+year;
+        }
+        System.out.println(leapDateStr);
+    }
+
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0) return true;
+        if (year % 100 == 0) return false;
+        if (year % 4 == 0) return true;
+        return false;
+    }
+}
